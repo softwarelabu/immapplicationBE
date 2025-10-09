f3519067e1904c6802b080d9230d981645560ca6
softearelabu <roberto.tenerelli.svil@gmail.com>
2025-10-09 10:36:48 +0200

    First Commit

---
+++ src/main/java/com/softlab/applicationHouse/ApplicationhouseApplication.java
@@ -0,0 +1,13 @@
+package com.softlab.applicationHouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationhouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationhouseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/softlab/applicationHouse/ApplicationhouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softlab.applicationHouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationhouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
